a65ca9149d53d5fb00ab38392e8d40ffe75887f0
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2018-03-25 13:08:22 +0300

    add support redux dev tool

--- src/store/storeConfig.js
+++ src/store/storeConfig.js
@@ -2,7 +2,10 @@ import { createStore } from 'redux'
 import rootReducer from '../reducer/index'
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState)
+  const store = createStore(
+    rootReducer,
+    initialState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
   if (module.hot) {
     module.hot.accept('../reducer', () => {
