8cae76f7058ae8f40a684ecd82654ee9f3aa43ba
Nebeschenko Igor <nebeschenkoichor@gmail.com>
2018-03-25 20:33:08 +0300

    rewrite reducers

--- src/reducer/cell.js
+++ src/reducer/cell.js
@@ -1,18 +1,24 @@
+import returnArray from '../helpers/return_array'
+
 const initialState = {
-  cell: 4
+  cell: 4,
+  cellArray: []
 }
+
+initialState.cellArray = returnArray(initialState.cell)
 
 export default function cell(state = initialState, action) {
   switch (action.type) {
     case 'INCREMENT_CELL':
       return {
         ...state,
-        cell: state.cell + action.payload
+        cellArray: returnArray(action.payload)
       }
     case 'DECREMENT_CELL':
+    const spliceCellArray = state.cellArray.splice(action.payload, 1)
       return {
         ...state,
-        cell: state.cell - action.payload
+        cellArray: state.cellArray
       }
     default:
       return state
--- src/reducer/row.js
+++ src/reducer/row.js
@@ -1,18 +1,24 @@
+import returnArray from '../helpers/return_array'
+
 const initialState = {
-  row: 4
+  row: 4,
+  rowArray: []
 }
+
+initialState.rowArray = returnArray(initialState.row)
 
 export default function row(state = initialState, action) {
   switch (action.type) {
     case 'INCREMENT_ROW':
       return {
         ...state,
-        row: state.row + action.payload
+        rowArray: returnArray(action.payload)
       }
     case 'DECREMENT_ROW':
+    const spliceRowArray = state.rowArray.splice(action.payload, 1)
       return {
         ...state,
-        row: state.row - action.payload
+        rowArray: state.rowArray
       }
     default:
       return state
